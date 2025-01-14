f17bca48bca40872fe453dd63378a492459b97ed
oneofthezombies <hunhoekim@gmail.com>
2025-01-14 04:11:33 +0000

    wip

--- build.zig
+++ build.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    unreachable;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "zigraphql",
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
 }
