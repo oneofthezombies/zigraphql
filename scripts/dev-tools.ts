2b09bfd8c23f1ea5189e0b3bd337f0c0859f2b41
oneofthezombies <hunhoekim@gmail.com>
2025-01-17 10:03:35 +0000

    wip

--- scripts/dev-tools.ts
+++ scripts/dev-tools.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { $ } from "bun";
+import fs from "node:fs";
+
+const REFERENCES_DIR = "references";
+await $`mkdir -p ${REFERENCES_DIR}`;
+if (!fs.existsSync(`${REFERENCES_DIR}/graphql-js`)) {
+  await $`git -C ${REFERENCES_DIR} clone --depth=1 --branch=v16.10.0 https://github.com/graphql/graphql-js.git"`;
+}
